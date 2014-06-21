4f01865e2b50e35a76f615a893bdfc7470a17007
mikehas <mikehas@gmail.com>
2014-06-20 20:51:35 -0400

    Initial Commit

---
+++ closeups_helper.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import sys
+from sets import Set
+
+# Given a set of letters and a word length, display all possible words of length n you can make with the original set of letters.
+
+# Input
+input = 'blabbeougotoum'
+length = 9
+
+# HELPERS
+
+# Find the number of occurences of each letter in a word
+def getCounts(word):
+  counts = {}
+  for i in word:
+    if counts.has_key(i):
+      counts[i] = counts[i] + 1
+    else:
+      counts[i] = 1
+  return counts
+
+# MAIN
+
+sup = Set(input)
+print sup
+print "Searching..."
+
+words = '/usr/share/dict/words'
+f = open(words)
+
+results = []
+
+# First pass 
+# Python Set comparisons only consider unique elements.
+for line in f.readlines():
+  line = line.strip()
+  if len(line) == length:
+    sub = Set(line) 
+    if sub.issubset(sup):
+      results.append(line)
+counts = getCounts(input)
+  
+results2 = []
+
+# Remove words from 1st pass that have extra letters
+for item in results:
+  itemcounts = getCounts(item)
+  candidate = True
+  for key, val in itemcounts.iteritems():
+    if candidate:
+      if val > counts[key]:
+        candidate = False
+  if candidate:
+    results2.append(item)
+
+for item in results2:
+  print item
+
